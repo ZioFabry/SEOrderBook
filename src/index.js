659491118ee781e0c25ca8c72885ce54e82e27db
ZioFabry <ziofabry@hotmail.com>
2019-11-02 01:41:51 +0100

    v1.0

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import cors from 'cors';
 
 var express = require('express');
 var app = express();
+var Client = require('node-rest-client').Client;
 
+const WebSocket = require('ws');
 const chalk = require('chalk');
 const log = console.log;
 
@@ -23,13 +25,193 @@ const logger = {
 };
 
 logger.info("Starting program...");
+logger.info("Retrieving Markets...");
+
+var options = {
+    mimetypes: {
+        json: ["application/json"]
+    }
+};
+
+var markets = new Map();
+var marketsId = new Map();
+
+var client = new Client(options);
+
+client.get(process.env.MARKETS_URL, function (data, response) {
+    // parsed response body as js object
+    //logger.debug("REST_data",data);
+    var arr = JSON.parse(data);
+    arr.forEach(function(entry) {
+        markets.set( entry[0]+'-'+entry[1], {id: entry[2], subscribed: false, bid: new Map(), ask: new Map(), lastRequest: 0 });
+        marketsId.set( entry[2], entry[0]+'-'+entry[1]);
+    });
+});
+
+const ws = new WebSocket(process.env.WSS_URL);
+
+ws.on('open', function open() {
+    logger.debug('WS.open()');
+});
+  
+ws.on('message', function incoming(data) {
+    var msg = JSON.parse(data);
+
+    switch(msg.k)
+    {
+        case "book":
+            msg.v.forEach( function(book) {
+                var id = book.m;
+
+                if(marketsId.has(id))
+                {
+                    var pair = marketsId.get(id);
+                    var market = markets.get(pair);
+
+                    logger.debug('Snapshot of market',pair,'id',id,'received');
+
+                    book.b.forEach( function(entry) {
+                        if( entry.b )
+                        {
+                            market.bid.set(entry.p,entry.a);
+                        } else {
+                            market.ask.set(entry.p,entry.a);
+                        }
+                    });
+
+                    ws.send( JSON.stringify({k: 'subscribe', v: id}) );
+
+                    market.subscribed = true;
+                } else {
+                    logger.error('book: market',id,'not found');
+                }
+            });
+            break;
+
+        case "bookdelta":
+            msg.v.forEach( function(delta) {
+                var id = delta.m;
+
+                if(marketsId.has(id))
+                {
+                    var pair = marketsId.get(id);
+                    var market = markets.get(pair);
+
+                    if( delta.b )
+                    {
+                        if( delta.v == 0)
+                        {
+                            market.bid.del(delta.p);
+                        } else {
+                            market.bid.set(delta.p,delta.v);
+                        }
+                    } else {
+                        if( delta.v == 0)
+                        {
+                            market.ask.del(delta.p);
+                        } else {
+                            market.ask.set(delta.p,delta.v);
+                        }
+                    }
+
+                    var sinceLastReq = Date.now() - market.lastRequest;
+                    logger.debug('Delta of market',pair,'id',id,'received (lastReq '+sinceLastReq+'):',JSON.stringify(delta));
+
+                    if( sinceLastReq > (5*60*1000) ) // Unsubscribe after 5 min of inactivity
+                    {
+                        market.subscribed = false;
+                        market.ask = new Map();
+                        market.bid = new Map();
+                        market.lastRequest = 0;
+                        
+                        ws.send( JSON.stringify({k: 'unsubscribe', v: id}) );
+                        logger.warn('Market',pair,'unsubscribed for inactivity');
+                    }
+                } else {
+                    logger.error('bookdelta: market',id,'not found');
+                }
+            });
+            break;
+    
+        default:
+            logger.debug('WS.message('+data+')');
+            break;
+    }
+});
 
 app.use(cors());
 
-app.get("/url", (req, res, next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-   });
-   
-app.listen(process.env.PORT, () => {
-    logger.info("Server running on port",process.env.PORT);
+app.get("/subscribe/:coin/:basecoin", (req, res, next) => {
+    var coin = req.params.coin;
+    var basecoin = req.params.basecoin;
+    var paircoin = coin+'-'+basecoin;
+
+    logger.info('REST: GET /book/'+coin+'/'+basecoin)
+
+    if( markets.has(paircoin) )
+    {
+        var pair = markets.get(paircoin);
+
+        if( !pair.subscribed )
+        {
+            ws.send( JSON.stringify({k: 'request', v: pair.id}) );
+        }
+
+        pair.lastRequest = Date.now();
+
+        res.json({success: 1});
+    } else {
+        logger.error('REST: market '+paircoin+' not found')
+        res.json({error: 'market '+paircoin+' not found'});
+    }
 });
+
+app.get("/book/:coin/:basecoin", (req, res, next) => {
+    var coin = req.params.coin;
+    var basecoin = req.params.basecoin;
+    var paircoin = coin+'-'+basecoin;
+
+    logger.info('REST: GET /book/'+coin+'/'+basecoin)
+
+    if( markets.has(paircoin) )
+    {
+        var pair = markets.get(paircoin);
+        if( !pair.subscribed )
+        {
+            ws.send( JSON.stringify({k: 'request', v: pair.id}) );
+            ws.send( JSON.stringify({k: 'subscribe', v: pair.id}) );
+
+            res.json({error: "Subscribing... please retry"});
+        } else {
+            pair.lastRequest = Date.now();
+
+            var response = { BuyOrders: [], SellOrders:[] };
+
+            const sortAsc  = (a,b) => a[0] > b[0] ? 1 : -1;
+            const sortDesc = (a,b) => a[0] < b[0] ? 1 : -1;
+            
+            var buy  = new Map([...pair.bid].sort(sortDesc));
+            var sell = new Map([...pair.ask].sort(sortAsc));
+
+            var idx = 0;
+            for (let [key, value] of buy) {
+                response.BuyOrders.push( {Index: idx++, Amount: value, Price: key});
+            }
+
+            idx = 0;
+            for (let [key, value] of sell) {
+                response.SellOrders.push( {Index: idx++, Amount: value, Price: key});
+            }
+
+            res.json(response);
+        }
+    } else {
+        logger.error('REST: market '+paircoin+' not found')
+        res.json({error: 'market '+paircoin+' not found'});
+    }
+});
+
+app.listen(process.env.PORT, () => {
+    logger.info('REST: Server running on port',process.env.PORT);
+});
+
